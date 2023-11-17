e3074278decdecb188183d2104b7ce717562a22c
Gilford0306 <Gilford0306@gmail.com>
2023-11-17 17:07:44 +0200

    Add project files.

---
+++ SimpleJWT/Model/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleJWT.Model
+{
+    public class UserDTO
+    {
+        public required string Username { get; set; } 
+        public required string Password { get; set; } 
+    }
+}
